19cf57ded744225dd281cdf15b00dd4353a4f027
Samiiz <skboss159@gmail.com>
2024-02-28 15:27:29 +0900

    create video's views & path

--- app/videos/urls.py
+++ app/videos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Videolist.as_view(), name='video-list')
+    path('', views.Videolist.as_view(), name='video-list'),
+    path('<int:pk>/', views.VideoDetail.as_view(), name='video-detail'),
 ]
--- app/videos/views.py
+++ app/videos/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from .models import Video
 from .serializers import VideoSerializer
 
@@ -14,4 +15,24 @@ class Videolist(APIView):
         return Response(serializer.data)
 
     def post():
+        pass
+
+class VideoDetail(APIView):
+    def get_object(self, pk):
+        # return get_object_or_404(Video, pk=pk)
+        try:
+            return Video.objects.get(pk=pk)
+        except Video.DoesNotExist:
+            raise NotFound
+    def get(self, request, pk):
+        video = self.get_object(pk)
+
+        serializer = VideoSerializer(video)
+
+        return Response(serializer.data)
+    def post():
+        pass
+    def put():
+        pass
+    def delete():
         pass
