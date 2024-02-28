65681588801b42a329cf9fa3ca65de3ca0f4979f
Samiiz <skboss159@gmail.com>
2024-02-28 10:28:21 +0900

    create common models

--- app/common/models.py
+++ app/common/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class CommonModel(models.Model):
+    # 데이터 생성시간기준 변경 불가
+    created_at = models.DateTimeField(auto_now_add=True)
+    # 데이터 생성시간기준 데이터 수정시 수정시간 기준으로 변동 
+    updated_at = models.DateTimeField(auto_now=True)
+
+# DB 테이블에 추가 안되도록 정의
+class Meta:
+    abstract = True
