4fb0a617588d5ebb7e67a47992b2fa103953a1c8
Samiiz <skboss159@gmail.com>
2024-02-28 11:32:08 +0900

    create Notification models

--- app/notifications/models.py
+++ app/notifications/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from common.models import CommonModel
+from users.models import User
+from videos.models import Video
 
-# Create your models here.
+class Notification(CommonModel):
+    message = models.CharField(max_length=255)
+    is_read = models.BooleanField(default=False)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.SET_NULL)
