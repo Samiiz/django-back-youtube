52c8e57420515cdcdb4ee8c889641a2207208dff
Samiiz <skboss159@gmail.com>
2024-02-28 11:21:06 +0900

    create Subcription models

--- app/subscriptions/models.py
+++ app/subscriptions/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from common.models import CommonModel
+from users.models import User
 
-# Create your models here.
+class Subcription(CommonModel):
+    subscriber = models.ForeignKey(User, on_delete=models.CASCADE)
+    subscribed_to = models.ForeignKey(User, on_delete=models.CASCADE)
