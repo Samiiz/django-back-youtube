de6886e6173250b9084bfa879a277d7a4d7c605f
Samiiz <skboss159@gmail.com>
2024-02-28 15:24:26 +0900

    modified path

--- app/app/urls.py
+++ app/app/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/v1/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('api/v1/videos', include('videos.urls')),
+    path('api/v1/videos/', include('videos.urls')),
 ]
