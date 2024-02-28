d0e9a4947fb6506a481e4d5f0b00391e9a3110ea
Samiiz <skboss159@gmail.com>
2024-02-28 11:18:03 +0900

    create comment models

--- app/comments/models.py
+++ app/comments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from common.models import CommonModel
+from users.models import User
+from videos.models import Video
 
-# Create your models here.
+
+class Comment(CommonModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+    like = models.PositiveIntegerField(default=0)
+    dislike = models.PositiveIntegerField(default=0)
+    content = models.TextField()
