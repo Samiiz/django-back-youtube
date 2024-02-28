d823deb5ad6ed891b9eeabdb90615cb4df72779c
Samiiz <skboss159@gmail.com>
2024-02-28 11:13:20 +0900

    create reaction models

--- app/reactions/models.py
+++ app/reactions/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from common.models import CommonModel
+from users.models import User
+from videos.models import Video
 
-# Create your models here.
+class Reaction(CommonModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+    LIKE = 1
+    DISLIKE = -1
+    NO_REACTION = 0
+
+    REACTION_CHOICES = (
+        (LIKE, 'like'),
+        (DISLIKE, 'Dislike'),
+        (NO_REACTION, 'No Reaction'),
+    )
+
+    reaction = models.IntegerField(
+        choices=REACTION_CHOICES,
+        default=NO_REACTION
+    )
