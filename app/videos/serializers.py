f3ec55b61de871171bb994153f3577d2e9ce5edd
Samiiz <skboss159@gmail.com>
2024-02-28 15:26:39 +0900

    create VideoSerializer

--- app/videos/serializers.py
+++ app/videos/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Video
+from users.serializers import UserSerializer
+from comments.serializers import CommentSerializer
+
 
 class VideoSerializer(ModelSerializer):
+    user = UserSerializer()
+    comment_set = CommentSerializer(many=True)
     class Meta:
         model = Video
         fields = '__all__'
