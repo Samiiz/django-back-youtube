aa4075bdb30b4f15fe2382ec5fd520629683f2f2
Samiiz <skboss159@gmail.com>
2024-02-28 10:39:08 +0900

    create video models

--- app/videos/models.py
+++ app/videos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from common.models import CommonModel
+from users.models import User
 
 # Create your models here.
+class Video(CommonModel):
+    title = models.CharField(max_length=255)
+    link = models.URLField()
+    desription = models.TextField(blank=True)
+    category = models.CharField(max_length=255)
+    views_count = models.PositiveIntegerField(default=0)
+    thumbnail = models.URLField()
+    video_uploaded_url = models.URLField()
+    video_file = models.FileField(upload_to='storage/')
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
